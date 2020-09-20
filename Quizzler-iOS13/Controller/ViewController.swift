255d6bfa5e7391dc4ee7c83cfbf0726eb0b332b4
aaadityaaaa <aaadityaaazz@gmail.com>
2020-09-20 23:32:49 +0530

    latest commit

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @objc func questions(){
+        questionLabel.text = quizBrain.getQuestionText()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+    }
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    
+    
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        questions()
+        
+    }
+    
+    @IBAction func actionPressed(_ sender: UIButton) {
+        
+        
+        let userAnswer = sender.currentTitle!
+        
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        // let actualAnswer = quizQuestion[questionNumber].answer
+        
+        if userGotItRight{
+            
+            sender.backgroundColor = UIColor.green
+            
+        }
+        else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target:self, selector: #selector(questions), userInfo:nil, repeats: false)
+        
+        
+    }
+    
+}
---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,25 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Aaditya Singh on 23/08/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+//making a new class
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q : String, a : String) {
+        text = q
+       answer = a 
+    }
+}
+
+
+
+
+
+
